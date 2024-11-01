a7693dfdab99d4941630832ca03848bb2d5ff4fb
mompei <ogunborjerry@gmail.com>
2024-11-01 11:02:10 +0100

    changed file name

--- programs/prime-nest/src/contexts/mod.rs
+++ programs/prime-nest/src/contexts/mod.rs
@@ -7,5 +7,5 @@ pub use premature_close::*;
 pub mod initialize_rewards_config;
 pub use initialize_rewards_config::*;
 
-pub mod payout_operations;
-pub use payout_operations::*;
+pub mod claim_rewards_and_close;
+pub use claim_rewards_and_close::*;
