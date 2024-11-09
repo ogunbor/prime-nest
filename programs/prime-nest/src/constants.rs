35660596a415d3882feff4ffbbb8f4da712964b6
mompei <ogunborjerry@gmail.com>
2024-11-09 08:55:19 +0100

    create constant.rs file

---
+++ programs/prime-nest/src/constants.rs
@@ -0,0 +1,7 @@
+use anchor_lang::prelude::*;
+
+#[constant]
+
+pub const LAMPORTS_PER_SOL: u64 = 1_000_000_000;
+pub const MAXIMUM_AGE: u64 = 30; // 30 secs
+pub const SOL_ID: &str = "0xef0d8b6fda2ceba41da15d4095d1da392a0d2f8ed0c6c7bc0f4cfac8c280b56d"; // SOL/USD price feed id
